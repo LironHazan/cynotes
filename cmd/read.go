8c0d69b64015201bea19a8c4aeaddd90388dcecc
lironh <lironenk@gmail.com>
2022-09-03 14:40:34 +0300

    fix(read): removed redundant template

--- cmd/read.go
+++ cmd/read.go
@@ -6,7 +6,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// readCmd represents the read command
 var readCmd = &cobra.Command{
 	Use:   "read",
 	Short: "Choose a revision to read",
@@ -22,14 +21,4 @@ var readCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(readCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// readCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// readCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
